3c44425e446d97ea7115d105b0625be3ab3a5549
Huw Fulcher <huw.fulcher@ons.gov.uk>
2018-06-29 15:19:50 +0100

    Set up script to work fully

--- geocode.py
+++ geocode.py
@@ -2,19 +2,25 @@ from geopy.geocoders import GoogleV3
 import pandas as pd
 import config as cfg
 
+# API key stored off repo for security purposes (although it's not that dangerous)
 API_KEY = cfg.API_KEY
 
 data = pd.read_excel("./data/libraries.xls")
 
-geolocator = GoogleV3(api_key=API_KEY)
-
-subset = data.head(10)
+# Setting up the geolocator to connect to Google's Geocoding API, biasing results to the UK by specifying the domain,
+# timeout is to stop there being too many requests a second and making things difficult.
+geolocator = GoogleV3(api_key=API_KEY, domain="maps.google.co.uk", timeout=100)
 
 lat = []
 lon = []
 missing = 0
+count = 0
 
+# Loop grabs each library, extracts the useful address data and passes this to the Google API to return location data
+# Lat and Lon are then grabbed and stored.
+# TODO Get the whole location raw dump and save that elsewhere? Just in case we want to investigate anything else
 for _, row in data.iterrows():
+    count += 1
     address = "{}, {}, {}, {}".format(row["TITLE"], row["STREET_ADDRESS"], row["TOWN"], row["POSTCODE"])
     location = geolocator.geocode(address)
 
@@ -25,6 +31,9 @@ for _, row in data.iterrows():
     else:
         lat.append(location.latitude)
         lon.append(location.longitude)
+
+    if (count % 25) == 0:
+        print("Addresses processed: {}".format(count))
 
 print("Number of missing addresses: {}".format(missing))
 
