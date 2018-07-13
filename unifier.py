c3237db571e73770f5c3146f4502f5e3ab3ae3ee
Huw Fulcher <huw.fulcher@ons.gov.uk>
2018-07-13 16:47:29 +0100

    Created a seemingly cleverer way of filtering out empty fields

--- unifier.py
+++ unifier.py
@@ -1,4 +1,5 @@
 def unify(data, columns):
-    data["ADDRESS"] = data[columns].apply(lambda x: ', '.join(filter(None, x.map(str))), axis=1)
+    # data["ADDRESS"] = data[columns].apply(lambda x: ', '.join(filter(None, x.map(str))), axis=1)
+    data["ADDRESS"] = data[columns].apply(lambda x: ', '.join(x.dropna().map(str)), axis=1)
 
     return data
